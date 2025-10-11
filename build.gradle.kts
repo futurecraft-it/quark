9f16373eb70c008504584faf53bb3a068f192bbf
danieleguglietti <gugliettidaniele@gmail.com>
2025-10-11 16:51:56 +0200

    ⚙️ ci/cd: added auto publisher

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,10 @@
+import org.jetbrains.dokka.gradle.DokkaTask
+
 plugins {
     `java-library`
     alias(libs.plugins.dokka)
     alias(libs.plugins.kotlin)
+    alias(libs.plugins.maven.publish)
     alias(libs.plugins.serialization)
 }
 
@@ -17,21 +20,64 @@ repositories {
 }
 
 dependencies {
-    implementation(libs.kache)
     implementation(libs.coroutines)
 
     implementation(libs.bundles.kotlin)
     implementation(libs.bundles.serialization)
 
     compileOnly(libs.bundles.minecraft)
-
-    // testImplementation(kotlin("test"))
 }
-
-//tasks.test {
-//    useJUnitPlatform()
-//}
 
 kotlin {
     jvmToolchain(21)
+}
+
+dokka {
+    moduleName = "quark"
+
+    dokkaPublications.html {
+        suppressInheritedMembers.set(true)
+        failOnWarning.set(true)
+        outputDirectory.set(layout.buildDirectory.dir("docs"))
+    }
+
+    pluginsConfiguration.html {
+        footerMessage.set("FUTURECRAFT (c) 2025")
+        customStyleSheets.from("assets/logo-styles.css")
+        customAssets.from("assets/logo-icon.png", "assets/logo-icon.svg")
+    }
+}
+
+mavenPublishing {
+    publishToMavenCentral()
+
+    signAllPublications()
+
+    coordinates(group.toString(), "quark", version.toString())
+
+    pom {
+        name = "Quark"
+        description = "A paper minecraft library."
+        inceptionYear = "2025"
+        url = "https://github.com/futurecraft-it/quark/"
+        licenses {
+            license {
+                name = "GNU Affero General Public License v3.0"
+                url = "https://www.gnu.org/licenses/agpl-3.0.txt"
+                distribution = "https://www.gnu.org/licenses/agpl-3.0.txt"
+            }
+        }
+        developers {
+            developer {
+                id = "danieleguglietti"
+                name = "Daniele Guglietti"
+                url = "https://github.com/danieleguglietti/"
+            }
+        }
+        scm {
+            url = "https://github.com/futurecraft-it/quark/"
+            connection = "scm:git:git://github.com/futurecraft-it/quark/.git"
+            developerConnection = "scm:git:ssh://git@github.com/futurecraft-it/quark/.git"
+        }
+    }
 }
